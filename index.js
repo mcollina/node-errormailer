1249c1bddeedaa2a1fdf33c957bec0b1751e1a4f
Matteo Collina <matteo.collina@gmail.com>
2013-01-04 11:13:08 +0100

    Simplified index.js using async.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var path           = require('path');
 var templatesDir   = path.join(__dirname, 'templates');
 var emailTemplates = require('email-templates');
-var _ = require("underscore");
+var _              = require("underscore");
+var async          = require("async");
 
 var env = process.env.NODE_ENV || 'development';
 
@@ -31,42 +32,36 @@ module.exports = function errormailer(transport, opts) {
 
     var mail = _.clone(opts);
 
-    emailTemplates(templatesDir, function(err, template) {
+    async.waterfall([
+      async.apply(emailTemplates, templatesDir),
+      function(template, callback) {
+        var locals = {
+          subject: opts.subject
+        };
 
+        if(typeof errorToBeSent == "string") {
+          locals.message = errorToBeSent;
+          locals.stack = "";
+        } else {
+          locals.message = errorToBeSent.message;
+          locals.stack = errorToBeSent.stack;
+        }
+        locals.req = req;
+        locals._ = _;
+
+        template('basic_error', locals, callback);
+      },
+      function(html, text, callback) {
+        mail.text = text;
+        mail.html = html;
+        transport.sendMail(mail, callback);
+      }
+    ], function(err) {
       if(err) {
         console.log(err);
         console.log(errorToBeSent.toString());
-        return;
       }
-
-      var locals = {
-        subject: opts.subject
-      };
-
-      if(typeof errorToBeSent == "string") {
-        locals.message = errorToBeSent;
-        locals.stack = "";
-      } else {
-        locals.message = errorToBeSent.message;
-        locals.stack = errorToBeSent.stack;
-      }
-      locals.req = req;
-      locals._ = _;
-
-      template('basic_error', locals, function(err, html, text) {
-
-        mail.text = text;
-        mail.html = html;
-
-        transport.sendMail(mail, function(err) {
-          if (err) {
-            console.log(err);
-            console.log(errorToBeSent.toString());
-          }
-          next();
-        });
-      });
-
+      next();
     });
   };
 };
